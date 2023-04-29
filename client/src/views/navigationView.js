ce125a4c5bf77bdea7a3345be5bc5c96908eeb6e
igachev <ivophoenix1135@gmail.com>
2023-04-29 17:14:37 +0300

    added navigation template

---
+++ client/src/views/navigationView.js
@@ -0,0 +1,28 @@
+import {html} from '../../node_modules/lit-html/lit-html.js'
+
+const loggedUserLinks = html `
+<li><a href="/logout">Logout</a></li>
+<li><a href="/create">Create Movie</a></li>
+`;
+
+const guestUserLinks = html `
+<li><a href="/login">Login</a></li>
+<li><a href="/register">Register</a></li>
+`;
+
+const navigationTemplate = (user) => html `
+        <nav>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/movies">All Movies</a></li>
+                ${user
+                  ? loggedUserLinks
+                  : guestUserLinks
+                  }
+            </ul>
+        </nav>
+`
+
+export async function navigationView(ctx) {
+    return navigationTemplate(ctx.user)
+}
