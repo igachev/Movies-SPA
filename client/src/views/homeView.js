b377be5804d425de3764ddc7536311649f048711
igachev <ivophoenix1135@gmail.com>
2023-04-30 13:59:48 +0300

    added style to home page

--- client/src/views/homeView.js
+++ client/src/views/homeView.js
@@ -1,9 +1,14 @@
 import {html} from '../../node_modules/lit-html/lit-html.js'
 
 const homeTemplate = () => html `
-<section>
-<h1>Lorem ipsum dolor sit amet.</h1>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore maiores placeat quasi nobis quidem sunt iure dignissimos dolores eligendi nam.</p>
+<section class="home-section">
+<h1>Welcome!</h1>
+<div class="options">
+<p>Browse through our movie / tv show collection and find out your next movie to watch</p>
+<p>Like and comment on the movies you enjoy</p>
+<p>Add Movies</p>
+<p>Create your own watchlist</p>
+</div>
 </section>
 `
 
