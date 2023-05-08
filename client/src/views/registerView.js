0e1a4138cf1cf671b8ef0babe9d03344904f7741
igachev <ivophoenix1135@gmail.com>
2023-05-08 23:13:00 +0300

    added style to register page

--- client/src/views/registerView.js
+++ client/src/views/registerView.js
@@ -2,18 +2,18 @@ import {html} from '../../node_modules/lit-html/lit-html.js'
 import * as userService from '../services/userService.js'
 
 const registerTemplate = (submitHandler) => html `
-<section>
-    <form @submit=${submitHandler} method="post">
+<section class="register-section">
+    <form @submit=${submitHandler} method="post" class="register-form">
         <label for="email">Email:</label>
-        <input type="email" name="email" id="email" autocomplete="off">
+        <input type="email" name="email" id="email" autocomplete="off" placeholder="john@gmail.com...">
 
         <label for="password">Password:</label>
-        <input type="password" name="password" id="password">
+        <input type="password" name="password" id="password" placeholder="****">
 
         <label for="repeatPassword">Repeat Password:</label>
-        <input type="password" name="repeatPassword" id="repeatPassword">
+        <input type="password" name="repeatPassword" id="repeatPassword" placeholder="****">
 
-        <input type="submit" value="Register">
+        <input type="submit" value="Register" class="btn">
 
         <hr>
         <p>Already Registered? <a href="/login">Click here</a></p>
